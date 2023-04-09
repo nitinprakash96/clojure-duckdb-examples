8a11b7c6cd081c09c6b4d44b815fa1eec478f36a
Nitin Prakash <prakash.nitin63@gmail.com>
2023-04-09 08:56:27 +0530

    update readme

--- README.md
+++ README.md
@@ -4,4 +4,20 @@ Just laying down some examples on how to use duckdb with clojure.
 
 ## Usage
 
-FIXME
+```clojure
+cloduck.core> (use 'cloduck.core :reload)
+nil
+cloduck.core> (run-pragma :version)
+{:library_version "v0.7.1", :source_id "b00b93f0b1"}
+cloduck.core> (run-pragma :database-size)
+{:free_blocks 0,
+ :memory_usage "0 bytes",
+ :database_name "file",
+ :wal_size "0 bytes",
+ :block_size 262144,
+ :memory_limit "6.8GB",
+ :total_blocks 3,
+ :used_blocks 3,
+ :database_size "786KB"}
+cloduck.core>
+```
--- src/cloduck/core.clj
+++ src/cloduck/core.clj
@@ -1,6 +1,18 @@
 (ns cloduck.core
   (:require [next.jdbc :as jdbc]))
 
+(def pragma-list
+  {:database-list "database_list"
+   :show-tables "show_tables"
+   :show-tables-expanded "show_tables_expanded"
+   :functions "functions"
+   :database-size "database_size"
+   :collations "collations"
+   :version "version"
+   })
+
+(defn pragma-stmt [key]
+  (str "PRAGMA " key))
 
 (defn ds []
   (jdbc/get-datasource {:dbtype "duckdb" :dbname "file.db" :host :none}))
@@ -9,6 +21,6 @@
   (with-open [conn (jdbc/get-connection (ds))]
     (jdbc/execute! conn ["CREATE TABLE test (a INTEGER, b VARCHAR)"])))
 
-(defn run-pragma [pragma-stmt]
+(defn run-pragma [key]
   (with-open [conn (jdbc/get-connection (ds))]
-    (first (jdbc/execute! conn [(str "PRAGMA " pragma-stmt)]))))
+    (first (jdbc/execute! conn [(pragma-stmt (key pragma-list))]))))
